318f31910a021d24e4f7b29d8b8068eff64cf7e3
gustirizqikarima <c050423026@mahasiswa.ac.id>
2024-12-26 15:37:08 +0800

    u: masukkan data

--- struct.cpp
+++ struct.cpp
@@ -22,5 +22,20 @@ cout<<"Masukan angka :";
 }
 
 mahasiswa sikc[30];
-
 int pos=-1;
+
+void masukanData(){
+pos++;
+system("cls");
+fflush(stdin);
+cout << "Masukkan NIM: ";
+getline (cin, sikc[pos].nim);
+cout << "Masukkan Nama: ";
+getline (cin, sikc[pos].nama);
+cout << "Masukkan Alamat: ";
+getline (cin, sikc[pos].alamat);
+cout << "Masukkan IPK: ";
+cin >> sikc[pos].ipk;
+cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
